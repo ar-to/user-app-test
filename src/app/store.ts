62a420a226955fc8e89a5150d5d29d4eb46284de
ar-to <aricode00@gmail.com>
2021-02-27 19:14:54 -0600

    setup root reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,18 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+
+import rootReducer from './rootReducer'
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+  reducer: rootReducer
+})
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
   unknown,
   Action<string>
 >;
+
+
+export type AppDispatch = typeof store.dispatch
