f68303db8ee7cc9396805a1fa8defc98a0fc039f
ar-to <aricode00@gmail.com>
2021-03-03 15:40:35 -0600

    fixed enzyme to work with react 17

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,5 +2,11 @@
 import "@testing-library/jest-dom/extend-expect";
 // enzyme
 import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+// import Adapter from "enzyme-adapter-react-16";
+/**
+ * Experimental due to delay in enzyme support for react 17 at the time of building this application
+ * @see https://www.npmjs.com/package/@wojtekmaj/enzyme-adapter-react-17
+ * @see https://github.com/enzymejs/enzyme/issues/2429#issuecomment-679265564
+ */
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 configure({ adapter: new Adapter() });
